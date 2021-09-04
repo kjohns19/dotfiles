bb17e00ff569b2c4b45c7353aac913187000c444
Kevin Johnson <krjohnson111@gmail.com>
2021-09-04 16:44:31 -0400

    Use python3.9 and finish type annotations for check-format, git pre-commit, and install.py

--- install.py
+++ install.py
@@ -1,18 +1,18 @@
-#!/usr/bin/env python3.8
+#!/usr/bin/env python3.9
 
 import argparse
 import os
 import pathlib
 
 
-def main():
+def main() -> None:
     args = parse_args()
     print(f'Installing to {args.directory}')
 
     root = pathlib.Path(__file__).parent.resolve()
     links = [
-        (l, '.' + l)
-        for l in [
+        (link, '.' + link)
+        for link in [
             'ackrc',
             'bash_profile',
             'bashrc',
@@ -25,8 +25,7 @@ def main():
         ]
     ]
     for srcname, destname in links:
-        make_link(root, root.joinpath(srcname), args.directory.joinpath(destname),
-                  dryrun=args.dryrun)
+        make_link(root, root / srcname, args.directory / destname, dryrun=args.dryrun)
 
     dirs = [
         ('bash', '.bash'),
@@ -37,11 +36,10 @@ def main():
         ('vim/swapfiles', '.vim/swapfiles')
     ]
     for srcname, destname in dirs:
-        make_dir(root, root.joinpath(srcname), args.directory.joinpath(destname),
-                 dryrun=args.dryrun)
+        make_dir(root, root / srcname, args.directory / destname, dryrun=args.dryrun)
 
 
-def parse_args():
+def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
@@ -57,7 +55,8 @@ def parse_args():
     return args
 
 
-def make_dir(root, src, dest, dryrun=False):
+def make_dir(root: pathlib.Path, src: pathlib.Path, dest: pathlib.Path,
+             dryrun: bool = False) -> None:
     print(f'{src.relative_to(root)} => {dest}')
     if (dest.is_symlink() or dest.exists()) and not dest.is_dir():
         backup(dest, dryrun=dryrun, indent=1)
@@ -68,7 +67,8 @@ def make_dir(root, src, dest, dryrun=False):
             make_link(root, f, dest.joinpath(f.name), dryrun=dryrun, indent=1)
 
 
-def make_link(root, src, dest, dryrun=False, indent=0):
+def make_link(root: pathlib.Path, src: pathlib.Path, dest: pathlib.Path,
+              dryrun: bool = False, indent: int = 0) -> None:
     tabs = '  ' * indent
     print(f'{tabs}{src.relative_to(root)} => {dest}')
     directory = dest.parent
@@ -83,7 +83,7 @@ def make_link(root, src, dest, dryrun=False, indent=0):
         dest.symlink_to(src)
 
 
-def backup(dest, dryrun=False, indent=0):
+def backup(dest: pathlib.Path, dryrun: bool = False, indent: int = 0) -> None:
     tabs = '  ' * indent
     bak = dest.with_name(dest.name + '.bak')
     if bak.is_symlink() or bak.exists():
